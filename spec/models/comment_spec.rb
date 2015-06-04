4c653e6378e546a1c4ffb7b2e07359771b9b0e4c
Guillaume Perrin <gperrin01@gmail.com>
2015-06-04 19:24:08 +0100

    done basic comment test: have a text

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'must have a text' do 
+    c = Comment.new
+    expect(c.valid?).to be false
+  end
 end
